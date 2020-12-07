dc012720459b434f6db51d17c1089915fa4f1b43
CerteKim <Seelkadoow@live.com>
2020-12-07 17:28:04 +0800

    1.1.3

--- customize.sh
+++ customize.sh
@@ -107,10 +107,10 @@ ui_print "- Generate module.prop"
 rm -rf $MODPATH/module.prop
 touch $MODPATH/module.prop
 echo "id=xray" > $MODPATH/module.prop
-echo "name=Xray4magisk" >> $MODPATH/module.prop
-echo -n "version=Module v1.1.2, Core " >> $MODPATH/module.prop
+echo "name=Xray4Magisk" >> $MODPATH/module.prop
+echo -n "version=Module Tun_v1.1.3, Core " >> $MODPATH/module.prop
 echo ${latest_xray_version} >> $MODPATH/module.prop
-echo "versionCode=20201205" >> $MODPATH/module.prop
+echo "versionCode=20201207" >> $MODPATH/module.prop
 echo "author=CerteKim" >> $MODPATH/module.prop
 echo "description=xray core with service scripts for Android" >> $MODPATH/module.prop
 
